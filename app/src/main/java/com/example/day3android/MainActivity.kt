22baaac7cae5860bd9487671f9546f25caf35754
aalherz <aalherz86@gmail.com>
2023-12-05 18:53:27 +0300

    nav bar finished

---
+++ app/src/main/java/com/example/day3android/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.day3android
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment()
+        val settingsFragment = SettingsFragment()
+        val profileFragment = ProfileFragment()
+
+        val navbar = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setCurrentFragment(homeFragment)
+
+        navbar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> setCurrentFragment(homeFragment)
+                R.id.profile -> setCurrentFragment(profileFragment)
+                R.id.settings -> setCurrentFragment(settingsFragment)
+            }
+            true
+        }
+    }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.framLayoutFrags,fragment)
+            commit()
+
+        }
+}
